d59d260cfbb42717000879f5772d5698e6d84458
unknown <aqeelmemon46@gmail.com>
2024-09-06 01:04:44 +0530

    updated justdetection

---
+++ copyanddelete.py
@@ -0,0 +1,37 @@
+import shutil
+import os
+
+
+
+def copy_and_delete_folder():
+    
+    src_folder = r'.//runs//classify//predict'
+    dest_folder = r'.//detections'
+
+    if not os.path.exists(src_folder):
+        print(f"Source folder '{src_folder}' does not exist.")
+        return
+
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+        print(f"Destination folder '{dest_folder}' created.")
+
+    try:
+        for item in os.listdir(src_folder):
+            src_item = os.path.join(src_folder, item)
+            dest_item = os.path.join(dest_folder, item)
+
+            if os.path.isdir(src_item):
+                shutil.copytree(src_item, dest_item)
+            else:
+                shutil.copy2(src_item, dest_item)
+
+       # print(f"All files from '{src_folder}' copied to '{dest_folder}'.")
+
+        shutil.rmtree("runs")
+        #print(f"Source folder '{src_folder}' deleted.")
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+
--- justimagedetection.py
+++ justimagedetection.py
@@ -1,5 +1,12 @@
 from ultralytics import YOLO
 import cv2
+import copyanddelete
+import numpy as np
+
+# Example usage:
+
+
+
 
 
 model = YOLO('.//model//drdetectionmodel.pt')
@@ -7,5 +14,15 @@ model = YOLO('.//model//drdetectionmodel.pt')
 img = './/images//mild//16.jpg'
 img2 = cv2.imread(img)
 
-results = model('.//images//mild//15_left.jpeg',show=True)
+results = model('.//images//mild//15_left.jpeg')
+
+names = results[0].names
+probs = results[0].probs
+
+stageofdr = names[probs.top1]
+
+
+
+#copyanddelete.copy_and_delete_folder()
+
 cv2.waitKey(0)
